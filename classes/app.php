215fa458ad57c8511c63157eb6e913091cba01fd
Athanasios D. Panagiotidis <adpanagiotidis@gmail.com>
2022-10-02 21:27:11 +0100

    αδÎFoundations commit

---
+++ classes/app.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Pictorial;
+
+class Application
+{
+    /**
+     * The Pictorial framework version.
+     *
+     * @var string
+     */
+    const VERSION = '1.0.0';
+
+    /**
+     * Create a new Pictorial application instance.
+     *
+     * @param  string|null  $basePath
+     * @return void
+     */
+    function __construct($basePath = null)
+    {
+
+    }
+}
